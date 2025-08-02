cc4f55b20bc0f9a851279c9daf76964282834823
PshaoDylan <yanxi829@gmail.com>
2025-08-02 21:48:54 +0800

    Delete content_script.js

--- content_script.js
+++
@@ -1,5 +0,0 @@
-// This script is intentionally left blank.
-// Its purpose is to be declared in the manifest.json to grant <all_urls>
-// permission for the chrome.scripting.executeScript API to work from the
-// background script. This is a more modern and secure approach than
-// having a persistent content script running on all pages.
